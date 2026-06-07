2ba7fcd0ccaab600a057a4e3ba8105b8618529da
Prathamesh779 <prathameshpangarkar@gmail.com>
2026-06-07 12:22:59 +0530

    Time: 24 ms (5.22%), Space: 12.3 MB (90.93%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+
+        symbols = ["M","CM", "D", "CD","C","XC","L" ,"XL","X","IX","V","IV","I"]
+
+        res = []
+        for value, symbols in zip(values, symbols):
+            count,num = divmod(num, value)
+            res.append(symbols * count)
+
+        return "".join(res)
