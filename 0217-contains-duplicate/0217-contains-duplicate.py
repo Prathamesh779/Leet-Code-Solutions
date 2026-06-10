0990fb97cb3808248a1046316e64f4dd1245c444
Prathamesh779 <prathameshpangarkar@gmail.com>
2026-06-10 16:09:29 +0530

    Time: 19 ms (60.53%), Space: 25.9 MB (56.56%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        s = set()
+
+        for n in nums:
+            if n in s:
+                return True
+            s.add(n)
+
+        return False
